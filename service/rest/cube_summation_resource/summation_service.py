4e9022a614d46ebf1378f214e16cb1e2f4c196fe
adiktivo <adiktivo@adiktivos-iMac.local>
2017-02-20 18:55:59 -0500

    RA: adding service layer - typo

--- service/rest/cube_summation_resource/summation_service.py
+++ service/rest/cube_summation_resource/summation_service.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/cube_summation', methods=['GET', 'POST'])
 def cube_summation():
-    #TODO: add logic to call cube summation
+    # TODO: add logic to call cube summation
     return ''
+
 
 if __name__ == 'main':
     app.run()
